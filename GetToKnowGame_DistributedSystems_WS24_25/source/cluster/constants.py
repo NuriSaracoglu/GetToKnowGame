9139c18eeacd3237ab668df8d92718c33dae1e7e
Nuri Saracoglu <63155196+NuriSaracoglu@users.noreply.github.com>
2025-01-24 15:48:52 +0100

    Update constants.py

--- GetToKnowGame_DistributedSystems_WS24_25/source/cluster/constants.py
+++ GetToKnowGame_DistributedSystems_WS24_25/source/cluster/constants.py
@@ -1,10 +1,16 @@
 import socket
 import ipaddress
 
-BROADCAST_PORT = 61424
-BUFFER_SIZE = 1024
-SUBNETMASK = "255.255.255.0"
+# Constants used in the program
+BROADCAST_PORT = 61424  # The port number used for broadcasting messages
+BUFFER_SIZE = 1024      # The buffer size for receiving data
+SUBNETMASK = "255.255.255.0"  # Subnet mask used to calculate the broadcast IP address
 
+# Get the IP address of the current machine
 IP_ADRESS_OF_THIS_PC = socket.gethostbyname(socket.gethostname())
+
+# Create an IPv4 network object using the current machine's IP and the subnet mask
 net = ipaddress.IPv4Network(IP_ADRESS_OF_THIS_PC + '/' + SUBNETMASK, False)
-BROADCAST_IP = net.broadcast_address.exploded
+
+# Calculate the broadcast IP address from the network object
+BROADCAST_IP = net.broadcast_address.exploded  # Get the broadcast IP address as a string
